cdb2f04336b830b4cf76d27cc73e2427989d9f6d
Raghavendra Rao Kulkarni <royalraghu53@gmail.com>
2025-11-16 21:52:43 +0530

    Update routes.jsx

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
-import ScrollToTop from "components/scrollToTop";
+import ScrollToTop from "components/ScrollToTop";
 import ErrorBoundary from "components/Errorboundary";
 import NotFound from "pages/Notfound";
 import Landing from './pages/landing';
