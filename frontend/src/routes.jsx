6b072a888e44379698f3b46a576db8127e7d710a
Raghavendra Rao Kulkarni <royalraghu53@gmail.com>
2025-11-16 17:51:04 +0530

    Fix import casing for ScrollToTop and ErrorBoundary

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
-import ScrollToTop from "components/ScrollToTop";
-import ErrorBoundary from "components/ErrorBoundary";
+import ScrollToTop from "components/scrollToTop";
+import ErrorBoundary from "components/Errorboundary";
 import NotFound from "pages/Notfound";
 import Landing from './pages/landing';
 import LoginPage from './pages/login';
